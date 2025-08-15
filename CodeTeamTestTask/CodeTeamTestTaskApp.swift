3a35251a9282b65d95576f439039a9902a3d7724
Mikhailo <mishak.aaa@gmail.com>
2025-08-15 13:21:39 +0300

    Initial Commit

---
+++ CodeTeamTestTask/CodeTeamTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTeamTestTaskApp.swift
+//  CodeTeamTestTask
+//
+//  Created by Mike Panchenko on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeTeamTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
